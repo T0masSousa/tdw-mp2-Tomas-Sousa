044efec6d62fdf3f7a078dfa0c36b20f3b790a8a
Tomás Sousa <tomasrafael@ua.pt>
2024-11-25 09:54:00 +0000

    Remove SpeedInsights wrapper from App component

--- mp2_tomas_sousa/src/App.js
+++ mp2_tomas_sousa/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar.js';
-import { SpeedInsights } from '@vercel/speed-insights/react';
 
 //PAGES
 import HomePage from './Pages/HomePage.js';
@@ -10,18 +9,16 @@ import ResultDetailsPage from './Pages/ResultDetailsPage.js';
 
 function App() {
   return (
-    <SpeedInsights>
-      <Router>
-        <div>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/film/:title" element={<ResultDetailsPage />} />
-          </Routes>
-        </div>
-      </Router>
-    </SpeedInsights>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/film/:title" element={<ResultDetailsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
